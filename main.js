e8880b0865fa280575eda4dad1af52b9d280fd73
dadjan <dadjankhan7@gamil.com>
2026-04-17 10:47:08 +0500

    adnan

---
+++ main.js
@@ -0,0 +1,49 @@
+
+const students = [
+    { name: "Amit", marks: 92, attendance: 95 },
+    { name: "Sara", marks: 45, attendance: 60 },
+    { name: "John", marks: 78, attendance: 80 },
+    { name: "Priya", marks: 88, attendance: 88 },
+    { name: "Mike", marks: 100, attendance: 100 },
+    { name: "Rahul", marks: 55, attendance: 90 }
+];
+
+function calculateGrade(marks) {
+    if (marks >= 90) {
+        return "A+";
+    } else if (marks >= 80) {
+        return "A";
+    } else if (marks >= 70) {
+        return "B";
+    } else if (marks >= 60) {
+        return "C";
+    } else {
+        return "Fail";
+    }
+}
+
+
+function checkScholarship(marks, attendance) {
+    if ((marks > 80 && attendance > 90) || (marks === 100)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.log("--- Student Report ---");
+students.forEach(function(student) {
+    const grade = calculateGrade(student.marks);
+    const isScholar = checkScholarship(student.marks, student.attendance);
+    
+    let statusMessage = "";
+    
+    if (isScholar === true) {
+        statusMessage = "Scholarship Eligible";
+    } else {
+        statusMessage = "Not Eligible";
+    }
+    
+    console.log(`Name: ${student.name} - Grade: ${grade} - Status: ${statusMessage}`);
+});
